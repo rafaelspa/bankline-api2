d73de6284b60c60009ef3f7d4170c4ef8f4e3cd9
rafaelspa <rsdepaula67@gmail.com>
2022-05-05 14:46:12 -0300

    Criamos as classes de modelo da aplicacao

---
+++ src/main/java/com/dio/santander/banklineapi2/model/Movimentacao.java
@@ -0,0 +1,36 @@
+package com.dio.santander.banklineapi2.model;
+
+import java.time.LocalDateTime;
+
+public class Movimentacao {
+	private Integer id;
+	private LocalDateTime dataHora;
+	private String descricao;
+	private Double valor;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public LocalDateTime getDataHora() {
+		return dataHora;
+	}
+	public void setDataHora(LocalDateTime dataHora) {
+		this.dataHora = dataHora;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Double getValor() {
+		return valor;
+	}
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+	
+	
+}
